2ea21e7ec18bb7cfc5e52c8a858336e4ad659c4c
jiaopaner <807854982@qq.com>
2019-11-06 11:46:23 +0800

    updare lib name

--- FaceRecognition/app/src/main/java/com/wisesoft/wiseface/MainActivity.java
+++ FaceRecognition/app/src/main/java/com/wisesoft/wiseface/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
 
     // Used to load the 'native-lib' library on application startup.
     static {
-        System.loadLibrary("native-lib");
+        System.loadLibrary("wiseface");
     }
 
     @Override
